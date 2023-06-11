0dbe9aa34ab5c37a17d96c8f2d1ce32716054e1a
baixianger <baixianger@gmail.com>
2023-06-11 09:56:32 +0200

    hpc

--- job_cpu.sh
+++ job_cpu.sh
@@ -31,7 +31,7 @@
 # python3 train.py wandb_group=ppo_multi task=MultiStepTaskSimple algo=ppo exp_name=reward_design_%J task.target=0 task.reward_type=0 algo.num_envs=64 algo.save_freq=500 algo.num_updates=3000
 # python3 train.py wandb_group=ppo_multi task=MultiStepTaskSimple algo=ppo exp_name=reward_design_%J task.target=0 task.reward_type=1 algo.num_envs=64 algo.save_freq=500 algo.num_updates=3000
 # python3 train.py wandb_group=ppo_multi task=MultiStepTaskSimple algo=ppo exp_name=reward_design_%J task.target=0 task.reward_type=2 algo.num_envs=64 algo.save_freq=500 algo.num_updates=3000
-python3 train.py wandb_group=ppo_multi task=MultiStepTaskSimple algo=ppo exp_name=reward_design_%J task.target=0 task.reward_type=3 algo.num_envs=64 algo.save_freq=500 algo.num_updates=3000
+# python3 train.py wandb_group=ppo_multi task=MultiStepTaskSimple algo=ppo exp_name=reward_design_%J task.target=0 task.reward_type=3 algo.num_envs=64 algo.save_freq=500 algo.num_updates=3000
 
 ############# 各种算法在多步任务模式下采用reward0规则的表现实验
 # python3 train.py wandb_group=multi_diff_algo task=MultiStepTaskSimple algo=ppo  exp_name=diff_ppo  task.target=0 task.reward_type=1 algo.num_envs=64 algo.save_freq=500  algo.num_updates=2000
